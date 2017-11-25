dd6613759a03e0815e8d854a5bfab3f3e6d04ff5
FUJI Goro (gfx) <gfuji@cpan.org>
2017-11-25 11:33:47 +0900

    fix restart.sh

--- restart.sh
+++ restart.sh
@@ -2,8 +2,8 @@
 set -xe
 cd /home/isucon/webapp/nodejs
 git pull
-npm install
-npm test
+~/local/node/npm install
+~/local/node/npm test
 sudo /usr/sbin/nginx -t
 sudo service nginx reload
 sudo systemctl restart cco.nodejs.service

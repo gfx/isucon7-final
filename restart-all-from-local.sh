d8e7f8a1fa8e42d2adac753b8bb0735f2a4a9969
FUJI Goro (gfx) <gfuji@cpan.org>
2017-11-25 11:30:27 +0900

    fix restart shell

--- restart-all-from-local.sh
+++ restart-all-from-local.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -xe
 ssh app0021 webapp/restart.sh
-ssh app0021 webapp/restart.sh
-ssh app0021 webapp/restart.sh
+ssh app0022 webapp/restart.sh
+ssh app0023 webapp/restart.sh
+ssh app0024 webapp/restart.sh

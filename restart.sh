ad808eccc625848dc74b8bb5f9075494adecd448
FUJI Goro (gfx) <gfuji@cpan.org>
2017-11-25 11:38:40 +0900

    fix paths

--- restart.sh
+++ restart.sh
@@ -2,8 +2,9 @@
 set -xe
 cd /home/isucon/webapp/nodejs
 git pull
-/home/isucon/local/node/bin/npm install
-/home/isucon/local/node/bin/npm test
+export PATH="/home/isucon/local/node/bin/:$PATH"
+npm install
+npm test
 sudo /usr/sbin/nginx -t
 sudo service nginx reload
 sudo systemctl restart cco.nodejs.service

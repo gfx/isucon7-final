edfde404c506080cc1f4ce96cb7e68af43f06891
Masahiro Nagano <kazeburo@gmail.com>
2017-11-25 13:06:19 +0900

    isert

--- nodejs/Game.ts
+++ nodejs/Game.ts
@@ -279,8 +279,7 @@ export default class Game {
   }
 
   async insertRoomTime() {
-    const connection = await this.pool.getConnection()
-    await connection.query('INSERT IGNORE INTO room_time(room_name, time) VALUES (?, 0)', [this.roomName])
+    await this.pool.query('INSERT IGNORE INTO room_time(room_name, time) VALUES (?, 0)', [this.roomName])
   }
 
   // 部屋のロックを取りタイムスタンプを更新する
@@ -290,6 +289,7 @@ export default class Game {
   // 状態になることに注意 (keyword: MVCC, repeatable read).
   async updateRoomTime (connection, reqTime) {
     // See page 13 and 17 in https://www.slideshare.net/ichirin2501/insert-51938787
+    await connection.query('INSERT INTO room_time(room_name, time) VALUES (?, 0) ON DUPLICATE KEY UPDATE time = time', [this.roomName])
     const [[{ time }]] = await connection.query('SELECT time FROM room_time WHERE room_name = ? FOR UPDATE', [this.roomName])
     const [[{ currentTime }]] = await connection.query('SELECT floor(unix_timestamp(current_timestamp(3))*1000) AS currentTime')
     if (parseInt(time, 10) > parseInt(currentTime, 10)) {
